7078efae309934cb335764223cfe9161efd790eb
Kelsey Low <kklowlow@gmail.com>
2019-04-26 03:16:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,10 +10,10 @@ class App < Sinatra::Base
   end
   
   post '/reverse' do
-  puts params
+    puts params
  
-  erb :reversed
-end
+    erb :reversed
+  end
 
   get '/friends' do
     # Write your code here!
